286bab04dc7d49161d4b5216786c48599e75c0d1
reinaldo-marin <80010987+reinaldo-marin@users.noreply.github.com>
2021-04-23 19:18:38 -0500

    Versión final del código

--- Parcial1.ino
+++ Parcial1.ino
@@ -2,62 +2,22 @@
 #define CLOCK_PIN 13
 #define SS_PIN_1 10
 #define SS_PIN_2 9
-int ch[5][8][8] =
-{  
-  {    
-    {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1}  
-  },
-  {    
+int ch[8][8] =
+{        
     {1, 1, 1, 1, 1, 1, 1, 1},
     {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 0, 0, 0, 0, 1, 1},
-    {1, 1, 0, 0, 0, 0, 1, 1},
-    {1, 1, 0, 0, 0, 0, 1, 1},
-    {1, 1, 0, 0, 0, 0, 1, 1},
     {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1}  
-  },
-  {    
-    {1, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 0, 0, 0},
-    {1, 1, 1, 1, 1, 0, 0, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0},
     {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1}  
-  },
-  {    
-    {1, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 1, 1, 1, 0, 0, 0},
-    {1, 1, 1, 1, 1, 0, 0, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0},
-    {1, 1, 0, 0, 0, 0, 0, 0},
     {1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1}  
-  },
-  {    
-    {0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 0, 0, 1, 1, 0, 0, 0},
-    {0, 0, 1, 1, 1, 1, 0, 0},
-    {1, 1, 1, 0, 0, 1, 1, 1},
-    {1, 1, 0, 0, 0, 0, 1, 1}  
-  }
-  
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}    
 };
-unsigned long timestamp = 0;
-int ch_no = 0;
+int matriz_modificable[8][8]{0};
+int *puntero_matriz = new int[8]{0};
+char col[8]{48,48,48,48,48,48,48,48};
+int fila = 1;
+char data;
 void spi_write_1(unsigned  char cData) {
   shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, cData);
   digitalWrite (SS_PIN_1, HIGH);
@@ -67,8 +27,8 @@ void spi_write_2( unsigned char cData) {
   digitalWrite (SS_PIN_2, HIGH);
   digitalWrite (SS_PIN_2, LOW);}
 void writePosition(int x, int y) {
-  spi_write_1(  0b00000001 << x );  // COL  
-    spi_write_2(~(0b00000001 << y )); // ROW
+  spi_write_1(  B00000001 << x );  // COL  
+    spi_write_2(~(B00000001 << y )); // ROW
 }
   void setup (){  
     pinMode(SS_PIN_1, OUTPUT);
@@ -76,27 +36,61 @@ void writePosition(int x, int y) {
     pinMode(DATA_PIN, OUTPUT);
     pinMode(CLOCK_PIN, OUTPUT);
     Serial.begin(9600);
-    Serial.println("Enter character : ");
-    timestamp = millis();
-    spi_write_1(  0b00000000);  // COL 
-    spi_write_2(~(0b00000000)); // ROW   
+    Serial.println("1. Verificacion de leds");
+    Serial.println("2. Crear tu propia imagen");
+    spi_write_1(  B00000000);  // COL 
+    spi_write_2(~(B00000000)); // ROW   
     }
   void loop(){
     if(Serial.available()==1){
       char t = Serial.read();
       Serial.println(t);
-      if(t=='t'){ch_no=0;}
-      if(t=='o'){ch_no=1;}
-      if(t=='f'){ch_no=3;}
-      if(t=='y'){ch_no=4;}
-      Serial.print("Enter character : ");
-    }
-    for (int row=0; row <8;row++) 
-    {
-      for (int col=0; col <8;col++) 
-      {if (ch[ch_no][row][col] == 1) {
-        writePosition(col,7-row);
-       }      
-      }    
+      if(t=='1'){
+    	for (int row=0; row <8;row++) 
+    	{
+  			for (int colu=0; colu <8;colu++) 
+  				{if (ch[colu][row] == 1) {
+   					writePosition(colu,row);
+  					}
+                }
+        }
+      }
+    Serial.print("Ingresa opcion");
+      if (t =='2'){
+        
+        while (fila < 8){
+          Serial.print("Ingrese la fila que quiere manipular");
+          fila = Serial.read();
+          Serial.print("Ingrese con 0 y 1 en orden para decidir cualquiere que encienda y cualquiere que se mantenga apagada, 1 para encender, 0 para mantener apagado");
+          col[1] = Serial.read();
+          for (int i = 0; i<8;i++){
+            *(puntero_matriz+i) = int(col[i]);
+          }
+          for (int i = 0; i<8; i++){
+            matriz_modificable[fila][i] = *(puntero_matriz+i);
+          }          
+          for (int row = 0; row < 8;row++){
+                        for (int colu=0; colu < 8;colu++){
+              if (matriz_modificable[row][colu]== 1){
+                writePosition(colu,row);
+              }
+            }
+          }
+          Serial.print("Quieres modificar otra fila? Escribe 'Y' en caso que si o 'N' si no");
+          data = Serial.read();
+          if (data == 'Y');
+          else if (data == 'N'){
+      		fila = 8;
+          }
+          while(data != 'Y' && data != 'N'){
+            Serial.print("Ingresa un caracter valido, Y para ingresar otra fila, N si quiere salir");
+            data = Serial.read();
+            if (data == 'Y');
+            else if (data == 'N'){
+              fila = 8;
+            }
+          }
+        }
+      }
     }
   }
